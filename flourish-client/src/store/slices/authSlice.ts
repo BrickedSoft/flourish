9e1aa45a8c169cc9cfd647ee250205272d3d773b
Yashrif Arifin <yaswoccho@gmail.com>
2023-09-08 03:05:20 +0600

    fix(authslice): fix user object

--- flourish-client/src/store/slices/authSlice.ts
+++ flourish-client/src/store/slices/authSlice.ts
@@ -1,15 +1,14 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import { Loading } from "../../types/Loading";
-import { User, userTypes } from "../../types/User";
+import { Status } from "../../types/Status";
+import { User } from "../../types/User";
 import { signIn, signUp } from "../actions/authActions";
 
 const initialState: User = {
   token: "",
-  type: userTypes.CLIENT,
   name: "",
   email: "",
-  loading: Loading.IDLE,
+  status: Status.IDLE,
 };
 
 const apiSlice = createSlice({
@@ -20,31 +19,31 @@ const apiSlice = createSlice({
     /* --------------------------------- Sign Up -------------------------------- */
 
     builder.addCase(signUp.pending, (state) => {
-      state.loading = Loading.PENDING;
+      state.status = Status.PENDING;
     });
 
     builder.addCase(signUp.fulfilled, (state) => {
-      state.loading = Loading.FULFILLED;
+      state.status = Status.FULFILLED;
     });
 
     builder.addCase(signUp.rejected, (state) => {
-      state.loading = Loading.REJECTED;
+      state.status = Status.REJECTED;
     });
 
     /* --------------------------------- Sign In -------------------------------- */
 
     builder.addCase(signIn.pending, (state) => {
-      state.loading = Loading.PENDING;
+      state.status = Status.PENDING;
     });
 
     builder.addCase(signIn.fulfilled, (state, action: PayloadAction<any>) => {
-      state.loading = Loading.FULFILLED;
+      state.status = Status.FULFILLED;
       console.log(action);
       state.token = action.payload;
     });
 
     builder.addCase(signIn.rejected, (state) => {
-      state.loading = Loading.REJECTED;
+      state.status = Status.REJECTED;
       state.token = "";
     });
   },
