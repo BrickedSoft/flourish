6b84db0133cc7451d4fca857d4207355ee3ddd5c
Jahid Hossain <54525656+jahidem@users.noreply.github.com>
2023-10-23 12:25:18 +0600

    feat: get counselor list

--- flourish-api/counselor/serializers.py
+++ flourish-api/counselor/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 
+from counselor.models import Counselor
+
 
 class SigninSerializer(serializers.Serializer):
     email = serializers.CharField()
     password = serializers.CharField()
 
+
 class SignupSerializer(serializers.Serializer):
     email = serializers.CharField()
     password = serializers.CharField()
+
+
+class CounselorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Counselor
+        fields = "__all__"
--- flourish-api/counselor/urls.py
+++ flourish-api/counselor/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ 
-  path("signup/", views.signup),
-  path("filledQuestionnaire/<str:clientId>/", views.FilledQuestionnaireView.as_view()),
-  path("questionnaire/", views.QuestionnaireView.as_view())
-  ]
+urlpatterns = [
+    path("signup/", views.signup),
+    path(
+        "filledQuestionnaire/<str:clientId>/", views.FilledQuestionnaireView.as_view()
+    ),
+    path("questionnaire/", views.QuestionnaireView.as_view()),
+    path("", views.CounselorView.as_view()),
+]
